553f90bfc04befeedec037c8b0dc063f10d32aaf
akirando <danielakiraando@gmail.com>
2015-10-30 21:08:21 -0200

    Create InitialFrameMain.java

---
+++ InitialFrameMain.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class InitialFrameMain {
+    
+    public static void main(String args[]) {
+
+        InitialFrame o = new InitialFrame();
+        o.setLocationRelativeTo(null);
+        o.setVisible(true);
+        o.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }
+}
